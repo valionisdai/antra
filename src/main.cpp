72e24e9b45c774027c8abdcf7823f5edb85c7120
Dainius <dainius1120@gmail.com>
2025-11-26 19:24:58 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "mylib.h"
+#include "globals.h"
+
 
 
 int main()
@@ -7,7 +9,7 @@ int main()
     cin.tie(nullptr);
 
     vector<string> failai_txt;
-    int a, b, c, d, e, g, h;
+    int a, c, d, e;
 
     cout << "Ar turite duomenis? (1 - taip, 2 - ne)" << endl;
     while(true) {if(cin >> d && d>=1 && d<=2) break; cout << "Neteisinga ivestis, pasirinkite 1 arba 2" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
@@ -27,7 +29,7 @@ int main()
             cout << "Generavimas baigtas!" << endl;
         }
     }
-    string folder = "./src", nfailas;
+    string folder = "./src/", nfailas;
         for(const auto& entry : fs::directory_iterator(folder))
         {
             if(entry.path().extension() == ".txt" && entry.path().filename().string().find("rez") == string::npos)
@@ -42,11 +44,24 @@ int main()
     cout << "Norite apskaiciuoti:\n(1) Vidurki\n(2) Mediana\n(3) Abu" << endl;
     while(true) {if(cin >> b && b>=1 && b<=3) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 3:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
 
+    cout << "Norite rikiuoti pagal:\n(1) Varda, (2) Pavarde, (3) Gal. Vid., (4) Gal. Med." << endl;
+    while(true) {if(cin >> g && g>=1 && g<=4) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 4:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
+    
+    cout << "Pasirinkite krypti:\n(1) Didejancia tvarka\n(2) Mazejancia tvarka" << endl;
+    while(true) {if(cin >> h && 0<h && h<3) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 2:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
+    
+    cout << "Kuria strategija norite naudoti rusiavimui?\n(1) Pirma\n(2) Antra\n(3) Trecia\n(4) Greiciausia abiems (skiriasi vektoriui ir sarasui)" << endl;
+    while(true) {if(cin >> f && f>=1 && f<=4) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 4:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
+    
+    cout << "Norite isvedimo i:\n(1) Faila\n(2) Konsole" << endl;
+    while(true){if(cin >> renkames && renkames>=1 && renkames<=2) break; cout << "Neteisinga ivestis, pasirinkite 1 arba 2" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
+
     if(d==1)
     {
         cout << "Norite:\n(1) Generuoti tik rezultatus automatiskai\n(2) Ranka suvesti rezultatus\n(3) Skaityti rezultatus is failo" << endl;
         while(true) {if(cin >> a && a>=1 && a<=3) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 3:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
     }
+    
     else if(d==2) a=3;
     if(a==3)
     {
@@ -63,48 +78,34 @@ int main()
             if(tst!=0)
                 cout << "Tokio failo nera, bandykite dar karta" << endl;
         }
-
-        cout << "Norite rusiuoti pagal:\n(1) Varda, (2) Pavarde, (3) Gal. Vid., (4) Gal. Med." << endl;
-        while(true) {if(cin >> g && g>=1 && g<=4) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 4:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
-
-        cout << "Pasirinkite krypti:\n(1) Didejancia tvarka\n(2) Mazejancia tvarka" << endl;
-        while(true) {if(cin >> h && 0<h && h<3) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 2:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
-
         if(e==1)
-            dirbam<vector>(b, nfailas, g, h);
+            dirbam<vector>(nfailas);
         else if(e==2)
-            dirbam<list>(b, nfailas, g, h);
+            dirbam<list>(nfailas);
     }
     else
     {
         int n;
         cout << "Iveskite studentu skaiciu:" << endl;
         while(true) {if(cin >> n && n>=1) break; cout << "Neteisinga ivestis, iveskite skaiciu" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
+        
         if(e==1)
         {
             vector<Studentas<vector>> Grupe;
             for(int i=1; i<=n; i++)
-                Grupe.push_back(skaiciuojam<vector>(a, b));
-            cout << "Norite rusiuoti pagal:\n(1) Varda, (2) Pavarde, (3) Gal. Vid., (4) Gal. Med." << endl;
-            while(true) {if(cin >> g && g>=1 && g<=4) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 4:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
-            cout << "Pasirinkite krypti:\n(1) Didejancia tvarka\n(2) Mazejancia tvarka" << endl;
-            while(true) {if(cin >> h && 0<h && h<3) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 2:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
-            rusiavimas<vector>(b, Grupe, "1", g, h);
+                Grupe.push_back(skaiciuojam<vector>(a));
+            kaip_rusiuojam<vector>(Grupe, "1");
         }
         else
         {
             auto start = chrono::high_resolution_clock::now();
             list<Studentas<list>> Grupe;
             for(int i=1; i<=n; i++)
-                Grupe.push_back(skaiciuojam<list>(a, b));
+                Grupe.push_back(skaiciuojam<list>(a));
             auto end = chrono::high_resolution_clock::now();
             chrono::duration<double> elaps = end - start;
             cout << "Skaiciavimo laikas: " << fixed << setprecision(3) << elaps.count() << " s" << endl;
-            cout << "Norite rusiuoti pagal:\n(1) Varda, (2) Pavarde, (3) Gal. Vid., (4) Gal. Med." << endl;
-            while(true) {if(cin >> g && g>=1 && g<=4) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 4:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
-            cout << "Pasirinkite krypti:\n(1) Didejancia tvarka\n(2) Mazejancia tvarka" << endl;
-            while(true) {if(cin >> h && 0<h && h<3) break; cout << "Neteisinga ivestis, pasirinkite skaiciu nuo 1 iki 2:" << endl; cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');}
-            rusiavimas<list>(b, Grupe, "1", g, h);
+            kaip_rusiuojam<list>(Grupe, "1");
         }
     }
     int kl;
